c0a7fad681f27a59a8f762b0d2cc76bf2d2f6ff1
dywy <dywy@users.noreply.github.com>
2018-10-15 22:03:17 +0200

    Initial Latex files

---
+++ code/MATLABFILE.m
@@ -0,0 +1,5 @@
+% MATLABFILE.m
+n = input('Enter a number:');
+for sentence = 1:n 				% have an indexing go from 1 to n
+    fprintf('%d. Hello world!\n', sentence);	% print the index, and a newline \n
+end
